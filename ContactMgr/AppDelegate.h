bca59b96d3e7b4db1a7f8d3018dc0609d672482d
Adam Bailey <baileya@sunysuffolk.edu>
2015-01-25 21:45:29 -0500

    Initial Commit

---
+++ ContactMgr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactMgr
+//
+//  Created by Adam Bailey on 1/25/15.
+//  Copyright (c) 2015 Adam Bailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContactMgr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactMgr
+//
+//  Created by Adam Bailey on 1/25/15.
+//  Copyright (c) 2015 Adam Bailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
